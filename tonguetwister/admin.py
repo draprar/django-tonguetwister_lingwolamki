2ba3e07b81ba45d363fe5da6be44e38fd889f063
Walery <waleron@gmail.com>
2024-04-03 23:36:53 +0200

    add templates, update admin, models, views (tonguetwister), urls (base) and requirements

--- tonguetwister/admin.py
+++ tonguetwister/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Post
+from .models import Apparatus, Articulation, Twister
 
-admin.site.register(Post)
+admin.site.register(Apparatus)
+admin.site.register(Articulation)
+admin.site.register(Twister)
--- tonguetwister/models.py
+++ tonguetwister/models.py
@@ -1,12 +1,39 @@
 from django.db import models
 
 
-class Post(models.Model):
+class Apparatus(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     text = models.TextField()
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.save()
 
     def __str__(self):
         return self.text
+
+
+class Articulation(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    text = models.TextField()
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.text
+
+
+class Twister(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    text = models.TextField()
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.text
+
+
