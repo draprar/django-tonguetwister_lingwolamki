32cc15110ee68b93b899ea271a0322251e2bca5b
Walery <waleron@gmail.com>
2024-04-03 17:08:21 +0200

    first commit

---
+++ tonguetwister/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    text = models.TextField()
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.text
