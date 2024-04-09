b6c0863033e1441ec211b0f41d02c33f59313230
Walery <waleron@gmail.com>
2024-04-09 23:47:31 +0200

    change templates&views (load more)

--- tonguetwister/views.py
+++ tonguetwister/views.py
@@ -4,7 +4,8 @@ from .models import Apparatus, Articulation, Twister
 
 def main(request):
     myset = {
-        "apparatus": Apparatus.objects.all(),
+        "apparatus": Apparatus.objects.all()[0:1],
+        "apparatus_next": Apparatus.objects.all()[1:3],
         "articulation": Articulation.objects.all(),
         "twister": Twister.objects.all(),
     }
