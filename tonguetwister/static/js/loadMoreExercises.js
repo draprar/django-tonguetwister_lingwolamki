bd28c07c41f87306b1e34478b244b80998287307
Walery <waleron@gmail.com>
2024-06-01 20:49:37 +0200

    cleaning

---
+++ tonguetwister/static/js/loadMoreExercises.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let loadMoreBtn = document.getElementById('load-more-exercises-btn');
+    let offset = parseInt(loadMoreBtn.getAttribute('data-offset'));
+    let url = loadMoreBtn.getAttribute('data-url');
+
+    loadMoreBtn.addEventListener('click', function() {
+        $.ajax({
+            url: url,
+            data: { 'offset': offset },
+            dataType: 'json',
+            success: function(data) {
+                if (data.length > 0) {
+                    for (let i = 0; i < data.length; i++) {
+                        $('#exercises-container').append('<div class="exercise col-md-16 fs-4">' + data[i].text + '</div>');
+                    }
+                    offset += data.length;
+                } else {
+                    $('#load-more-exercises-btn').hide();
+                }
+            }
+        });
+    });
+
+    function changeExercisesButtonText() {
+        var button = document.getElementById("load-more-exercises-btn");
+        button.textContent = "Więcej";
+    }
+    document.getElementById("load-more-exercises-btn").onclick = changeExercisesButtonText;
+});
---
+++ tonguetwister/static/js/loadMoreRecords.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let loadMoreBtn = document.getElementById('load-more-btn');
+    let offset = parseInt(loadMoreBtn.getAttribute('data-offset'));
+    let url = loadMoreBtn.getAttribute('data-url');
+
+    loadMoreBtn.addEventListener('click', function() {
+        $.ajax({
+            url: url,
+            data: { 'offset': offset },
+            dataType: 'json',
+            success: function(data) {
+                if (data.length > 0) {
+                    for (let i = 0; i < data.length; i++) {
+                        $('#records-container').append('<div class="record col-md-16 fs-4">' + data[i].text + '</div>');
+                    }
+                    offset += data.length;
+                } else {
+                    $('#load-more-btn').hide();
+                }
+            }
+        });
+    });
+
+    function changeRecordsButtonText() {
+        var button = document.getElementById("load-more-btn");
+        button.textContent = "Więcej";
+    }
+    document.getElementById("load-more-btn").onclick = changeRecordsButtonText;
+});
