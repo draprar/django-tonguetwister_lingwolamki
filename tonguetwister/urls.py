5e9f9ed7fbdd60875ae3df09d1de0889fe88e170
Walery <waleron@gmail.com>
2024-08-06 22:18:45 +0200

    add user's email reset

--- tonguetwister/urls.py
+++ tonguetwister/urls.py
@@ -3,6 +3,7 @@ from . import views
 from .views import login_view, register_view, activate
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -10,6 +11,17 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('register/', register_view, name='register'),
     path('activate/<uidb64>/<token>/', activate, name='activate'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'),
+         name='password_reset'),
+    path('password_reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
+         name='password_reset_complete'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('load-more-articulators/', views.load_more_articulators, name='load_more_articulators'),
     path('load-more-exercises/', views.load_more_exercises, name='load_more_exercises'),
