4e869db3551b25e9da8f6924f9d2b3ed8d85778b
Walery <waleron@gmail.com>
2024-08-31 15:05:36 +0200

    add host for custom domain

--- base/settings.py
+++ base/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = 'django-insecure-8*xd5qqgdj*7ac8m%!#h%8_y^(2wjs3ybu&@nks)0h48=0mmch
 DEBUG = True
 
 '''ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')'''
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'www.lingwolamki.online']
 
 # Application definition
 
