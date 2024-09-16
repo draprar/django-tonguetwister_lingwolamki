d3736dbceff63ae895955af1a07540f724154cda
Walery <waleron@gmail.com>
2024-09-17 00:34:26 +0200

    ddd unit models tests and GitHub Actions workflow for CI/CD integration

---
+++ tonguetwister/tests/test_models.py
@@ -0,0 +1,54 @@
+import pytest
+from django.contrib.auth.models import User
+from django.utils import timezone
+from datetime import timedelta
+from tonguetwister.models import (
+    Profile, Twister, Articulator, Exercise, Trivia, Funfact,
+    OldPolish, UserProfileTwister, UserProfileArticulator, UserProfileExercise
+)
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("model_class, text", [
+    (Twister, "Test Twister"),
+    (Articulator, "Test Articulator"),
+    (Exercise, "Test Exercise"),
+    (Trivia, "Test Trivia"),
+    (Funfact, "Test Funfact")
+])
+def test_model_str(model_class, text):
+    instance = model_class.objects.create(text=text)
+    assert str(instance) == text
+
+
+@pytest.mark.django_db
+def test_oldpolish_str():
+    old_polish = OldPolish.objects.create(old_text="staropolskie", new_text="nowomowa")
+    expected_str = "Czy wiesz, że staropolskie staropolskie można przetłumaczyć jako nowomowa?"
+    assert str(old_polish) == expected_str
+
+
+@pytest.mark.django_db
+def test_profile():
+    user = User.objects.create_user(username="testuser", password="test_password")
+    profile, created = Profile.objects.get_or_create(user=user)
+    assert str(profile) == "testuser Profile"
+
+    profile.last_login_date = timezone.now().date() - timedelta(days=1)
+    profile.save()
+    profile.update_login_streak()
+    assert profile.login_streak == 1
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("model_class, related_field, user_profile_model_class", [
+    (Twister, "twister", UserProfileTwister),
+    (Articulator, "articulator", UserProfileArticulator),
+    (Exercise, "exercise", UserProfileExercise),
+])
+def test_user_profile_relationship(model_class, related_field, user_profile_model_class):
+    user = User.objects.create_user(username="testuser", password="test_password")
+    related_instance = model_class.objects.create(text=f"Sample {model_class}.__name__")
+
+    user_profile_model = user_profile_model_class.objects.create(user=user, **{related_field: related_instance})
+    assert getattr(user_profile_model, related_field) == related_instance
