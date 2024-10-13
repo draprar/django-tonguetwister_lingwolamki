80b06e0f90c22f5e858c3c52590e8afaaca462b7
Walery <waleron@gmail.com>
2024-10-13 12:33:43 +0200

    brooming JS

--- tonguetwister/static/js/top.js
+++ tonguetwister/static/js/top.js
@@ -1,16 +1,13 @@
-let mybutton = document.getElementById("topBtn");
+const mybutton = document.getElementById("topBtn");
 
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
-    mybutton.style.display = "block";
+window.onscroll = () => {
+  if (window.pageYOffset > 1) {
+    myButton.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    myButton.style.display = "none";
   }
-}
+};
 
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+const topFunction = () => {
+  window.scrollTo({ top: 0, behavior: 'smooth'})
+};
