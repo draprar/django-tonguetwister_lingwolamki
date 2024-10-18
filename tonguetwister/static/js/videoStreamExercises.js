1d57d4867afbd5f836d2fe7794f3f8623a8842ec
Walery <waleron@gmail.com>
2024-10-18 21:13:20 +0200

    brooming JS (videoStreamArticulators.js,videoStreamExercises.js, videoStreamTwisters.js)

--- tonguetwister/static/js/videoStreamExercises.js
+++ tonguetwister/static/js/videoStreamExercises.js
@@ -3,16 +3,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const videoContainer = document.getElementById('video-container-exercises');
     const videoPreview = document.getElementById('video-preview-exercises');
 
-    let stream;
+    let stream = null;
 
+    // Toggle video stream when mirror button is clicked
     mirrorBtn.addEventListener('click', () => {
-        if (!videoContainer.style.display || videoContainer.style.display === 'none') {
-            startVideoStream();
-        } else {
-            stopVideoStream();
-        }
+        videoContainer.style.display === 'none' || !videoContainer.style.display
+            ? startVideoStream()
+            : stopVideoStream();
     });
 
+    // Start video stream using the user's webcam
     function startVideoStream() {
         navigator.mediaDevices.getUserMedia({ video: { facingMode: 'user' }, audio: false })
             .then(mediaStream => {
@@ -22,13 +22,15 @@ document.addEventListener('DOMContentLoaded', () => {
                 mirrorBtn.textContent = '🛑 Zatrzymaj lusterko';
             })
             .catch(error => {
-                console.error('Error accessing media devices.', error);
+                console.error('Error accessing media devices:', error);
             });
     }
 
+    // Stop the video stream and reset the interface
     function stopVideoStream() {
         if (stream) {
             stream.getTracks().forEach(track => track.stop());
+            stream = null;
             videoPreview.srcObject = null;
             videoContainer.style.display = 'none';
             mirrorBtn.textContent = '📷 Otwórz lusterko';
